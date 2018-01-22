3ef98c566b7f21e6813c10e4bffe479b39d18ddd
Lukas Elmer <lukas.elmer@gmail.com>
2018-01-22 01:13:43 +0100

    JS demo code

---
+++ README.md
@@ -0,0 +1,19 @@
+# TypeScript Code Samples
+
+
+```ts
+interface Person {
+  name: string;
+  age: number;
+}
+```
+
+```ts
+class Greeter {
+  name: string;
+  sayHello() {
+    console.log("Hello " + name);
+  }
+}
+```
+
---
+++ src/main.js
@@ -0,0 +1,19 @@
+function sortByName(arr) {
+  var result = arr.slice(0);
+  result.sort(function(a, b) {
+    return a.name.localCompare(b.name);
+  });
+  return result;
+}
+
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+//
+// class Greeter {
+//   name: string;
+//   sayHello() {
+//     console.log("Hello " + name);
+//   }
+// }
